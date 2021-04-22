7cb7cfc6deac96d0bdf297971c6ea10cabfdbf63
Michael Pazienza <michael@mashbox.com>
2021-04-22 16:47:09 -0500

    Update

--- src/js/views/intake/index.js
+++ src/js/views/intake/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { AUTHENTICATED } from '../../constants';
 
 // Components
-import { Header } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import IntakeList from '../../components/intake/list';
 
 // Create the view
@@ -19,6 +19,8 @@ class IntakeView extends Component {
     return (
       <div className="view view-intake">
         <Header as="h1">Intake</Header>
+
+        <Button primary>New Intake</Button>
 
         <IntakeList intakes={ intakes } />
       </div>
